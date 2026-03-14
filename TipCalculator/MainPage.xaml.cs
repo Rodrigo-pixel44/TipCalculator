d79846a7427ee0ed27b329486a75a75fe8aa17ad
Etec Prof. Horácio Augusto da Silveira <fleitasrodrigo31>
2026-03-13 22:38:58 -0300

    feat: adicionada lógica do calculo de gorjeta

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void CalcularButton_Clicked(object sender, EventArgs e)
+        {
+            // outra forma de realizar conversão
+            // double refeicao = Convert.ToDouble(RefeicaoEntry.Text);
+            double refeicao = Double.Parse(RefeicaoEntry.Text);
+            double porcentagem = Double.Parse(PorcentagemEntry.Text);
+            int qtdPessoas = Int32.Parse(QuantidadedePessoaEntry.Text);
+
+            double gorjeta = refeicao * (porcentagem / 100);
+            double valortotal = refeicao + gorjeta;
+            double valorPorPessoa = valortotal / qtdPessoas;
+
+            GorjetaEntry.Text = gorjeta.ToString();
+            ValorporPessoaEntry.Text = valorPorPessoa.ToString();
+            ValortotalEntry.Text = valortotal.ToString();
+
+
+
+
+        }
+
+
+
+    }
+}

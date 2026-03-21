2774fd371339b3dae5c33aea48168b183d09a5ab
Rodrigo-pixel44 <fleitasrodrigo31@gmail.com>
2026-03-20 22:25:09 -0300

    first commit

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace TipCalculator
+﻿using System.Diagnostics;
+
+namespace TipCalculator
 {
     public partial class MainPage : ContentPage
     {
@@ -8,26 +10,42 @@
         }
         private void CalcularButton_Clicked(object sender, EventArgs e)
         {
-            // outra forma de realizar conversão
-            // double refeicao = Convert.ToDouble(RefeicaoEntry.Text);
-            double refeicao = Double.Parse(RefeicaoEntry.Text);
-            double porcentagem = Double.Parse(PorcentagemEntry.Text);
-            int qtdPessoas = Int32.Parse(QuantidadedePessoaEntry.Text);
 
-            double gorjeta = refeicao * (porcentagem / 100);
-            double valortotal = refeicao + gorjeta;
-            double valorPorPessoa = valortotal / qtdPessoas;
+            try
+            {
+                // outra forma de realizar conversão
+                // double refeicao = Convert.ToDouble(RefeicaoEntry.Text);
+                double refeicao = Double.Parse(RefeicaoEntry.Text);
+                double porcentagem = Double.Parse(PorcentagemEntry.Text);
+                int qtdPessoas = Int32.Parse(QuantidadedePessoaEntry.Text);
 
-            GorjetaEntry.Text = gorjeta.ToString();
-            ValorporPessoaEntry.Text = valorPorPessoa.ToString();
-            ValortotalEntry.Text = valortotal.ToString();
+                double gorjeta = refeicao * (porcentagem / 100);
+                double valortotal = refeicao + gorjeta;
+                double valorPorPessoa = valortotal / qtdPessoas;
 
+                GorjetaEntry.Text = gorjeta.ToString("C");
+                ValorporPessoaEntry.Text = valorPorPessoa.ToString("C");
+                ValortotalEntry.Text = valortotal.ToString("C");
 
+            }
 
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
 
         }
 
+        //  private void CalcularButton_Clicked(object sender, EventArgs e);
 
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            PorcentagemEntry.Text = value.ToString();
+        }
 
     }
+
+
+
 }
